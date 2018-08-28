d742d93ab45523253220b7e9823b1723f69a279f
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-29 01:38:16 +0800

    Add io socket listener for user count increases

--- public/client.js
+++ public/client.js
@@ -2,7 +2,12 @@ $( document ).ready(function() {
   
   /*global io*/
   var socket=io()
-   
+ 
+  socket.on('user count', function (data) {
+    console.log(data);
+  });
+ 
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function(){
     var messageToSend = $('#m').val();
