8c05b463198e600bd38a2454537cdbc2f25bdcf4
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-29 01:01:37 +0800

    Add current user variable
    Instantiate io.emit()

--- server.js
+++ server.js
@@ -25,8 +25,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
-console.log(`Database is: ${process.env.DATABASE}`)
-
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -37,8 +35,10 @@ app.use(session({
 
 
 mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, conn) => {
+  
   const db = conn.db("wme-practicedb")
     if(err) console.log('Database error: ' + err);
+    else { console.log(`Connected to: ${process.env.DATABASE}`)}
   
     auth(app, db);
     routes(app, db);
@@ -46,9 +46,12 @@ mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, conn) => {
     http.listen(process.env.PORT || 3000);
 
   
-    //start socket.io code  
+    //start socket.io code
+    var currentUsers = 0  
     io.on('connection', socket => {
       console.log('A user has connected')
+      currentUsers++
+      io.emit(`user count: ${currentUsers}`)
     })
   
 
