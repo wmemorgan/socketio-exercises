3989a19ba837c8be195ac653f0556927e9f335bd
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-08-29 02:32:22 +0800

    Display number of users connected
    Display names of users entering and exiting the chat room

--- public/client.js
+++ public/client.js
@@ -6,6 +6,21 @@ $( document ).ready(function() {
   socket.on('user count', function (data) {
     console.log(data)
   });
+
+  socket.on('user', function (data) {
+    document.getElementById('num-users').innerText = data.currentUsers + ' users online'
+    var message = data.name;
+    if (data.connected) {
+      message += ' has joined the chat.';
+    } else {
+      message += ' has left the chat.';
+    }
+    var ul = document.getElementById("messages");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(message));
+    li.setAttribute("style", "font-weight:bold"); // added line
+    ul.appendChild(li);
+  });
  
 
   // Form submittion with new message in field with id 'm'
